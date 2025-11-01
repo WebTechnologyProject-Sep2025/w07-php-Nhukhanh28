cb616b8c12a39b24428f1308960b88cd8dd3b7ba
SWS01921 <106223829@student.swin.edu.au>
2025-11-01 22:00:11 +0700

    update

--- lab07-part1/daysarray.php
+++ lab07-part1/daysarray.php
@@ -3,29 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Days of the week</title>
+    <title>PHP Variables, Arrays and Operators</title>
 </head>
 <body>
+    <h1>PHP Variables, Arrays and Operators</h1>
+    <hr>
 <?php
     // Days in English
     $days = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 
-    echo "<h2>Days of the week (English)</h2>";
-    echo "<ul>";
-    foreach ($days as $day) {
-        echo "<li>$day</li>";
-    }
-    echo "</ul>";
+    echo "<h2>The days of the week in English are:</h2>";
+    echo "<p>" . implode(",", $days) . "</p>";
 
     // Days in French
      $days_fr = array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
 
-    echo "<h2>Jours de la semaine (French)</h2>";
-    echo "<ul>";
-    foreach ($days_fr as $day) {
-        echo "<li>$day</li>";
-    }
-    echo "</ul>";
+    echo "<h2>The days of the week in French are:</h2>";
+    echo "<p>" . implode(",", $days) . "</p>";
 ?>
 </body>
 </html>
